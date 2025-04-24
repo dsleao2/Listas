b927b15deb68cd9ddf6cff880b6a3eca5226ceea
Daniele Santana <dssleao2@icloud.com>
2025-04-24 19:33:20 -0300

    Ultimo elemento da lista
    Mostrando como se faz para buscar o ultimo elemento da lista.

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // criando listas e mexendo com indices
 
-let linguagem = ['javascript','java','python'];
-console.linguagem [1];
 
+//no console do navegador: 
+let linguagem ['javascript','java','python']
+linguagem[0]
+'javascript'
+
+// ou seja, sempre que eu quiser pegar o primeiro elemento da lista, eu preciso chama-lo de "0".
+
+// para solicitar que te mostre, o ultimo elemento da lista, deve-se fazer da seguinte forma:
+
+linguagem [ linguagem.length - 1]
+'python'
