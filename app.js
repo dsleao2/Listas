2ca0354d04ee5a8b2ef52cdaf40e3e418c48e820
Daniele Santana <dssleao2@icloud.com>
2025-04-24 20:39:14 -0300

    Resolução
    Resolução dos desafios, feitos referente as aulas sobre listas.

--- app.js
+++ app.js
@@ -12,3 +12,31 @@ linguagem[0]
 
 linguagem [ linguagem.length - 1]
 'python'
+
+//DESAFIO SOBRE LISTAS: 
+//1)crie uma lista, com o nome listaGenerica.
+
+let listaGenerica []
+
+//2) Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com o s seguintes elementos:'JavaScript','C','C++', 'Kotlin' e 'Python'.
+
+let linguagensDeProgramacao ['JavaScript','C','C++', 'Kotlin','Python']
+
+//3)Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+
+let linguagensDeProgramacao ['JavaScript','C','C++', 'Kotlin','Python']
+linguagensDeProgramacao.push ('Java','Ruby','Golang');
+
+//4)Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+
+let nome ['Dani','Alvaro','Ben']
+    console.log(nome [0]);
+
+//5)Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+
+    let nome ['Dani','Alvaro','Ben']
+    console.log(nome [1]);
+
+//Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[2]);
